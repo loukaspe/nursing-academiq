47f3556fa13a71a6c062da784d19d168b3138976
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2023-04-22 23:12:27 +0300

    swagger init

--- back-end/internal/handlers/healthCheckHandler.go
+++ back-end/internal/handlers/healthCheckHandler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"gorm.io/gorm"
 	"net/http"
 )
@@ -15,17 +16,52 @@ func NewHealthCheckHandler(db *gorm.DB) *HealthCheckHandler {
 	}
 }
 
+// swagger:model HealthCheckResponse
+type HealthCheckResponse struct {
+	Message string `json:"message"`
+}
+
+// swagger:operation GET /health-check healthCheck
+//
+// # Check for the health of the app
+//
+// ---
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+//	Schemes:
+//	- http
+//	- https
+//
+// responses:
+//   '200':
+//     description: api is ok
+//     schema:
+//       "$ref": "#/definitions/HealthCheckResponse"
+//   '500':
+//     description: api got problem
+//     schema:
+//       "$ref": "#/definitions/HealthCheckResponse"
+
 func (handler *HealthCheckHandler) HealthCheckController(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
+	response := HealthCheckResponse{}
+
 	if postgreSQL, err := handler.db.DB(); err != nil {
 		if err = postgreSQL.Ping(); err == nil {
-			w.Write([]byte(`{message:"db not connected"}`))
+			response.Message = "db not connected"
+			json.NewEncoder(w).Encode(response)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	}
 
-	w.Write([]byte(`OK`))
+	response.Message = "OK"
+	json.NewEncoder(w).Encode(response)
 	w.WriteHeader(http.StatusOK)
 }
